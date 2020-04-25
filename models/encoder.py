65dbb56ed3a95325c9df4d81266287df7888e4b1
yagnapriyad <42752286+yagnapriyad@users.noreply.github.com>
2020-04-25 14:57:01 -0700

    Add files via upload

---
+++ models/encoder.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+
+import torch
+import torchvision.models
+
+class Encoder(torch.nn.Module):
+    def __init__(self, cfg):
+        super(Encoder, self).__init__()
+        self.cfg = cfg
+
+        # Layer Definition
+        vgg16_bn = torchvision.models.vgg16_bn(pretrained=True)
+        self.vgg = torch.nn.Sequential(*list(vgg16_bn.features.children()))[:27]
+        self.layer1 = torch.nn.Sequential(
+            torch.nn.Conv2d(512, 512, kernel_size=3),
+            torch.nn.BatchNorm2d(512),
+            torch.nn.ELU(),
+        )
+        self.layer2 = torch.nn.Sequential(
+            torch.nn.Conv2d(512, 512, kernel_size=3),
+            torch.nn.BatchNorm2d(512),
+            torch.nn.ELU(),
+            torch.nn.MaxPool2d(kernel_size=3)
+        )
+        self.layer3 = torch.nn.Sequential(
+            torch.nn.Conv2d(512, 256, kernel_size=1),
+            torch.nn.BatchNorm2d(256),
+            torch.nn.ELU()
+        )
+
+        # Don't update params in VGG16
+        for param in vgg16_bn.parameters():
+            param.requires_grad = False
+
+    def forward(self, rendering_images):
+        rendering_images = rendering_images.permute(1, 0, 2, 3, 4).contiguous()
+        rendering_images = torch.split(rendering_images, 1, dim=0)
+        image_features = []
+
+        for img in rendering_images:
+            features = self.vgg(img.squeeze(dim=0))
+            features = self.layer1(features)
+            features = self.layer2(features)
+            features = self.layer3(features)
+            image_features.append(features)
+
+        image_features = torch.stack(image_features).permute(1, 0, 2, 3, 4).contiguous()
+        return image_features
